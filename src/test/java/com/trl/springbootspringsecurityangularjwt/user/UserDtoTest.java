c4203e7021b15816c5e37e7397007f52f7313549
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-10-13 15:09:25 +0200

    Add new functionality.

--- src/test/java/com/trl/springbootspringsecurityangularjwt/user/UserDtoTest.java
+++ src/test/java/com/trl/springbootspringsecurityangularjwt/user/UserDtoTest.java
@@ -4,17 +4,12 @@ import com.trl.springbootspringsecurityangularjwt.enumeration.Authority;
 import com.trl.springbootspringsecurityangularjwt.enumeration.Role;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
 class UserDtoTest {
 
     private UserDto userDto;
@@ -79,22 +74,53 @@ class UserDtoTest {
     @Test
     public void testGetters() {
         assertAll("Testing Getters Methods.",
-                () -> {assertEquals(id, userDto.getId());},
-                () -> {assertEquals(userId, userDto.getUserId());},
-                () -> {assertEquals(firstName, userDto.getFirstName());},
-                () -> {assertEquals(lastName, userDto.getLastName());},
-                () -> {assertEquals(username, userDto.getUsername());},
-                () -> {assertEquals(password, userDto.getPassword());},
-                () -> {assertEquals(email, userDto.getEmail());},
-                () -> {assertEquals(profileImageUrl, userDto.getProfileImageUrl());},
-                () -> {assertEquals(lastLoginDate, userDto.getLastLoginDate());},
-                () -> {assertEquals(lastLoginDateDisplay, userDto.getLastLoginDateDisplay());},
-                () -> {assertEquals(joinDate, userDto.getJoinDate());},
-                () -> {assertEquals(role, userDto.getRole());},
-                () -> {assertEquals(authorities, userDto.getAuthorities());},
-                () -> {assertEquals(isActive, userDto.isActive());},
-                () -> {assertEquals(isNotLocked, userDto.isNotLocked());}
+                () -> {assertEquals(id, userDto.getId(), "The getter that returns the user's 'id' does not work correctly");},
+                () -> {assertEquals(userId, userDto.getUserId(), "The getter that returns the user's 'userId' does not work correctly");},
+                () -> {assertEquals(firstName, userDto.getFirstName(), "The getter that returns the user's 'firstName' does not work correctly");},
+                () -> {assertEquals(lastName, userDto.getLastName(), "The getter that returns the user's 'lastName' does not work correctly");},
+                () -> {assertEquals(username, userDto.getUsername(), "The getter that returns the user's 'username' does not work correctly");},
+                () -> {assertEquals(password, userDto.getPassword(), "The getter that returns the user's 'password' does not work correctly");},
+                () -> {assertEquals(email, userDto.getEmail(), "The getter that returns the user's 'email' does not work correctly");},
+                () -> {assertEquals(profileImageUrl, userDto.getProfileImageUrl(), "The getter that returns the user's 'profileImageUrl' does not work correctly");},
+                () -> {assertEquals(lastLoginDate, userDto.getLastLoginDate(), "The getter that returns the user's 'lastLoginDate' does not work correctly");},
+                () -> {assertEquals(lastLoginDateDisplay, userDto.getLastLoginDateDisplay(), "The getter that returns the user's 'lastLoginDateDisplay' does not work correctly");},
+                () -> {assertEquals(joinDate, userDto.getJoinDate(), "The getter that returns the user's 'joinDate' does not work correctly");},
+                () -> {assertEquals(role, userDto.getRole(), "The getter that returns the user's 'role' does not work correctly");},
+                () -> {assertEquals(authorities, userDto.getAuthorities(), "The getter that returns the user's 'authorities' does not work correctly");},
+                () -> {assertEquals(isActive, userDto.isActive(), "The getter that returns the user's 'isActive' does not work correctly");},
+                () -> {assertEquals(isNotLocked, userDto.isNotLocked(), "The getter that returns the user's 'isNotLocked' does not work correctly");}
         );
+    }
+
+    @Test
+    public void testGettersFail() {
+        assertAll("Testing Getters Methods.",
+                () -> {assertNotEquals(id + 1, userDto.getId());},
+                () -> {assertNotEquals(userId + "XXX", userDto.getUserId());},
+                () -> {assertNotEquals(firstName + "XXX", userDto.getFirstName());},
+                () -> {assertNotEquals(lastName + "XXX", userDto.getLastName());},
+                () -> {assertNotEquals(username + "XXX", userDto.getUsername());},
+                () -> {assertNotEquals(password + "XXX", userDto.getPassword());},
+                () -> {assertNotEquals(email + "XXX", userDto.getEmail());},
+                () -> {assertNotEquals(profileImageUrl + "XXX", userDto.getProfileImageUrl());},
+                () -> {assertNotEquals(lastLoginDate.plusSeconds(1), userDto.getLastLoginDate());},
+                () -> {assertNotEquals(lastLoginDateDisplay.plusSeconds(1), userDto.getLastLoginDateDisplay());},
+                () -> {assertNotEquals(joinDate.plusSeconds(1), userDto.getJoinDate());},
+                () -> {assertNotEquals(Role.ROLE_USER, userDto.getRole());},
+                () -> {assertNotEquals(Authority.USER_AUTHORITIES, userDto.getAuthorities());},
+                () -> {assertNotEquals(!isActive, userDto.isActive());},
+                () -> {assertNotEquals(!isNotLocked, userDto.isNotLocked());}
+        );
+    }
+
+    @Test
+    public void testSetters() {
+
+    }
+
+    @Test
+    public void testSettersFail() {
+
     }
 
 }
